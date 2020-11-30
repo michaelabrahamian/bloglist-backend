0b78d7a23ea6aa825bb1a809552d49c1aa217377
Michael Abrahamian <michael.abrahamian7@gmail.com>
2020-11-30 21:37:51 +1100

    Added build files for front-end React code

---
+++ build/precache-manifest.acf4b131782da2bef126c0a905c1ce6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaaecd420f8b479960db69e2a8e3261d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7b8ac5db9a6fe022372",
+    "url": "/static/css/2.3e575b9f.chunk.css"
+  },
+  {
+    "revision": "87014dfbc40d5a54ca33",
+    "url": "/static/css/main.c62829e1.chunk.css"
+  },
+  {
+    "revision": "a7b8ac5db9a6fe022372",
+    "url": "/static/js/2.c88e6799.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.c88e6799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87014dfbc40d5a54ca33",
+    "url": "/static/js/main.d9e60948.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
